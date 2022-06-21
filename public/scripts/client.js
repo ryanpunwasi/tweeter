9b63e5532c278166f8ea822f330a04aff16af74c
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-21 18:08:38 -0400

    Implement renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,52 +4,74 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-    },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#main').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-});
-
 const createTweetElement = (tweet) => {
-  const { user } = tweet;
-  const { content } = tweet;
-  const created_at = tweet.created_at;
+  const { user } = tweet; // User information
+  const { text } = tweet.content; // Tweet text
+  const createdAt = tweet.created_at; // Date (in milliseconds) that the tweet was created
 
-  const tweetEle = $(
-  `<article class="tweet">
-    <header class="tweet-header">
-      <div class="tweet-header-left">
-        <img src="${ user.avatars }" />
-        <div class="name">${ user.name }</div>
-      </div>
-      <div class="handle">${ user.handle }</div>
-    </header>
-    <p class="tweet-body">${ content.text }</p>
-    <hr />
-    <footer class="tweet-footer">
-      <p>${ created_at }</p>
-      <div class="icon-bar">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>`);
+  // HTML template for tweet
+  const tweetEle =
+  $(
+    `<article class="tweet">
+      <header class="tweet-header">
+        <div class="tweet-header-left">
+          <img src="${ user.avatars }" />
+          <div class="name">${ user.name }</div>
+        </div>
+        <div class="handle">${ user.handle }</div>
+      </header>
+      <p class="tweet-body">${ text }</p>
+      <hr />
+      <footer class="tweet-footer">
+        <p>${ createdAt }</p>
+        <div class="icon-bar">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`
+  );
 
   return tweetEle;
 };
+
+const renderTweets = tweets => {
+  
+  // Create HTML template for each tweet and append it to #main
+  tweets.forEach(tweet => {
+    const tweetElement = createTweetElement(tweet);
+    $('#main').append(tweetElement);
+  });
+};
+
+// Test / driver code (temporary). Eventually will get this from the server.
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+$(document).ready(function() {
+  renderTweets(data);
+});
