104f621f22c06aa709fd13c3c8b0530a4fe3b9e4
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-21 00:52:34 -0400

    Add comments explaining each step

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
   const MAX_CHARS = 140;
+
+  // Event handling whenever the value of the element with id `tweet-text` changes
   $("#tweet-text").on('input', function() {
+
+    // Traverse DOM to find counter. Start at the element with id `tweet-text`, move to it's next immediate sibling, then find the output element in said sibling
     const counter = $(this).next().find("output");
+
+    // Update the number of characters left
     const charactersLeft = MAX_CHARS - this.value.length;
+
+    // Change counter color to red if character limit is exceeded
     if (charactersLeft < 0) {
       counter.addClass('red');
     } else if (counter.hasClass('red') && charactersLeft >= 0) {
       counter.removeClass('red');
     }
+
+    // Set inner text of counter to the number of characters left
     counter.html(charactersLeft);
   });
 });
