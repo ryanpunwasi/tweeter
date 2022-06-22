91f0ee475db07c7afabfc7ba2fa2739766fe0601
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-21 21:24:49 -0400

    Revise error message

--- public/scripts/validate.js
+++ public/scripts/validate.js
@@ -2,7 +2,7 @@ const validateTweetSubmission = input => {
   // Assign relevent error message/boolean based on value of input
   let error = 
   !input ? 'Please enter some text.' :
-  input.length > 140 ? 'Your tweet must be less than 140 characters.' :
+  input.length > 140 ? 'Your tweet must be 140 characters or less.' :
   false;
 
   return error;
