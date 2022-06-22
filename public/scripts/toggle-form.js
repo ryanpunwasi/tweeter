a56b3da682aff92bc311c3ea78e4eca0ed9ae57f
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-22 18:18:43 -0400

    Add comments, focus on textarea only when form is being shown

---
+++ public/scripts/toggle-form.js
@@ -0,0 +1,9 @@
+const toggleForm = () => {
+  const formSection = $(".new-tweet"); // Select section containing form
+  const textarea = $(formSection).find('#tweet-text'); // Select textarea in form
+  $(formSection).slideToggle(() => {
+    if (formSection.css("display") !== "none") { // Only focus when form is being shown
+      textarea.focus();
+    }
+  });
+};
