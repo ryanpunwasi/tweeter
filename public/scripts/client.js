c54012e7a269cce62771d32718f10976370a208b
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-21 20:32:04 -0400

    Remove test code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,7 +23,7 @@ const createTweetElement = (tweet) => {
       <p class="tweet-body">${ text || '' }</p>
       <hr />
       <footer class="tweet-footer">
-        <p>${ createdAt }</p>
+        <p>${ timeago.format(createdAt) }</p>
         <div class="icon-bar">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -41,36 +41,9 @@ const renderTweets = tweets => {
   // Create HTML template for each tweet and append it to #main
   tweets.forEach(tweet => {
     const tweetElement = createTweetElement(tweet);
-    $('#main').append(tweetElement);
+    $('#tweets-container').append(tweetElement);
   });
 };
-
-// Test / driver code (temporary). Eventually will get this from the server.
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_a": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 $(document).ready(function() {
   $(".new-tweet form").submit(function(event) {
@@ -82,5 +55,14 @@ $(document).ready(function() {
     });
   });
 
-  renderTweets(data);
+  const loadTweets = () => {
+    $.ajax('/tweets', { 
+      method: 'GET'
+    })
+    .done(function(data) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 });
