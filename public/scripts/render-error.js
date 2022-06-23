4d3311594a8eaa4be1149d37b28f1fd020a707c3
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-23 00:10:26 -0400

    Add comments, implement animation of DOM node passed in as argument

--- public/scripts/render-error.js
+++ public/scripts/render-error.js
@@ -1,13 +1,15 @@
-const renderError = (error) => {
-  const errorElement = $('.error'); // Get error element
+const renderError = (errorText, ele) => {
+/**
+ * Toggles animation for error element (ele) and sets text of error element to value of errorText
+ */
 
   // If previous error message is open, close it and update error message
-  errorElement.slideUp(() => {
-    errorElement.text(error);
+  ele.slideUp(() => {
+    ele.text(errorText);
   });
 
   // Show error
   if (error) {
-    errorElement.slideDown();
+    ele.slideDown();
   }
 };
