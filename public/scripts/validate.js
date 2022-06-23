02a4183fdcc49b22af644e534ff0d71563528606
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-23 19:39:36 -0400

    Revise comment

--- public/scripts/validate.js
+++ public/scripts/validate.js
@@ -1,9 +1,11 @@
 const validateTweetSubmission = input => {
-  // Assign relevent error message/boolean based on value of input
+/* 
+Return a string with error message if input is empty or greater than 140 characters long. Otherwise, return an empty string.
+*/ 
   let error =
-  !input ? 'Please enter some text.' :
-  input.length > 140 ? 'Your tweet must be 140 characters or less.' :
-  '';
+  !input ? 'Please enter some text.' 
+  : input.length > 140 ? 'Your tweet must be 140 characters or less.' 
+  : '';
 
   return error;
 };
