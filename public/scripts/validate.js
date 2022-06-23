aaabb7ea2692d5c8473b797cf58c8c9de1b8d998
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-23 00:29:28 -0400

    Remove extra space in error assignment

--- public/scripts/validate.js
+++ public/scripts/validate.js
@@ -1,6 +1,6 @@
 const validateTweetSubmission = input => {
   // Assign relevent error message/boolean based on value of input
-  let error = 
+  let error =
   !input ? 'Please enter some text.' :
   input.length > 140 ? 'Your tweet must be 140 characters or less.' :
   false;
